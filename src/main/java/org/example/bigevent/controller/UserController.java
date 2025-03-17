a22da7b2635499d7d42c71e9c3fdcaa1d6e3e93f
胡静怡 <2635567096@qq.com>
2025-03-17 17:15:54 +0800

    feat:线程

--- src/main/java/org/example/bigevent/controller/UserController.java
+++ src/main/java/org/example/bigevent/controller/UserController.java
@@ -6,13 +6,13 @@ import org.example.bigevent.pojo.User;
 import org.example.bigevent.service.UserService;
 import org.example.bigevent.utils.JwtUtil;
 import org.example.bigevent.utils.Md5Util;
+import org.example.bigevent.utils.ThreadLocalUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Author: Vivienne
@@ -63,6 +63,18 @@ public class UserController {
         }
 
         return Result.error("密码错误！！");
+    }
+
+    @GetMapping("/userInfo")
+    public Result<User> userInfo(/*@RequestHeader(name = "Authorization")String token*/){
+        //根据用户名查询用户
+        /*Map<String, Object> map = JwtUtil.parseToken(token);
+        String username = (String) map.get("username");*/
+
+        Map<String,Object> map = ThreadLocalUtil.get();
+        String username = (String) map.get("username");
+        User user = userService.findByUsername(username);
+        return Result.success(user);
     }
 
 }
--- src/main/java/org/example/bigevent/interceptors/LoginInterceptor.java
+++ src/main/java/org/example/bigevent/interceptors/LoginInterceptor.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.bigevent.pojo.Result;
 import org.example.bigevent.utils.JwtUtil;
+import org.example.bigevent.utils.ThreadLocalUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -22,11 +23,23 @@ public class LoginInterceptor implements HandlerInterceptor {
         String token = request.getHeader("Authorization");
         try {
             Map<String, Object> claims = JwtUtil.parseToken(token);
+
+
+            //把业务数据存储到ThreadLocal中
+            ThreadLocalUtil.set(claims);
+            //放行
             return true;
         }catch (Exception e){
             //http状态码401
             response.setStatus(401);
             return false;
         }
+    }
+
+    //防止内存泄露
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //清空ThreadLocal中的数据
+        ThreadLocalUtil.remove();
     }
 }
